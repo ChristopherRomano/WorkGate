2978c8199ed396739c027a419457e5fc1e23073a
CalamaroPotente <chrishromano@gmail.com>
2026-04-15 18:11:46 +0100

    implemented it ticket

--- WorkGate-backend/FDM/src/main/java/com/workgate/fdm/DTO/ItTicketUpdateRequest.java
+++ WorkGate-backend/FDM/src/main/java/com/workgate/fdm/DTO/ItTicketUpdateRequest.java
@@ -1,7 +1,20 @@
 package com.workgate.fdm.DTO;
 
 public class ItTicketUpdateRequest {
-    private int id;
-    private int name;
+    private long id;
+    private String name;
+    private String claimByEmail;
 
+    public ItTicketUpdateRequest() {}
+
+    public long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getClaimByEmail() {
+        return claimByEmail;
+    }
 }
--- WorkGate-backend/FDM/src/main/java/com/workgate/fdm/controller/ItTicketController.java
+++ WorkGate-backend/FDM/src/main/java/com/workgate/fdm/controller/ItTicketController.java
@@ -1,43 +1,53 @@
 package com.workgate.fdm.controller;
 
 import com.workgate.fdm.DTO.ItTicketUpdateRequest;
-import com.workgate.fdm.model.*;
-import com.workgate.fdm.repository.LeaveRequestRepository;
-
+import com.workgate.fdm.model.ItTicket;
+import com.workgate.fdm.model.STATUS;
+import com.workgate.fdm.repository.ItTicketRequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:5173")
 public class ItTicketController {
-    
+
     @Autowired
-    LeaveRequestRepository leaveRequestRepository;
+    ItTicketRequestRepository itTicketRepository;
 
+    // GET ALL TICKETS
     @RequestMapping("/itTickets")
-    public List<ItTicket> getTickets () 
-    {
-        return List.of();
+    public List<ItTicket> getTickets() {
+        return itTicketRepository.findAll();
     }
 
+    // CLAIM TICKET
     @RequestMapping("/claimItTicket")
-    public void claimTicket (@RequestBody ItTicketUpdateRequest request) 
-    {
-        
+    public void claimTicket(@RequestBody ItTicketUpdateRequest request) {
+
+        ItTicket ticket = itTicketRepository.findById(request.getId())
+                .orElseThrow(() ->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND, "Ticket not found"));
+
+        ticket.setClaimedByEmail(request.getClaimByEmail());
+
+        itTicketRepository.save(ticket);
     }
 
+    // RESOLVE TICKET
     @PostMapping("/resolveItTicket")
-    public void resolveTicket (@RequestParam int id) 
-    {
+    public void resolveTicket(@RequestParam long id, STATUS status) {
 
+        ItTicket ticket = itTicketRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND, "Ticket not found"));
+
+        ticket.updateStatus(status);
+
+        itTicketRepository.save(ticket);
     }
-        
-    
 }
