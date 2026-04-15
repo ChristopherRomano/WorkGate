f5ca921a7e8abd912db638921c6fc2a1ca8c68ca
Aayan Aboobakar <aayan22926@gmail.com>
2026-04-15 18:39:03 +0100

    fixed login again :(

--- WorkGate-backend/FDM/src/main/java/com/workgate/fdm/controller/AuthController.java
+++ WorkGate-backend/FDM/src/main/java/com/workgate/fdm/controller/AuthController.java
@@ -1,7 +1,5 @@
 package com.workgate.fdm.controller;
 
-import java.io.Console;
-
 import com.workgate.fdm.DTO.LoginResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
